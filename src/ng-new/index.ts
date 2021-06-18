e8d2c3b503c554cc5b9bef9cb721138c31f9a588
Shawn Dube <shawn_dube@hotmail.com>
2021-06-17 23:30:30 -0400

    use applyToSubtree

--- src/ng-new/index.ts
+++ src/ng-new/index.ts
@@ -1,9 +1,15 @@
+import { normalize } from '@angular-devkit/core';
 import {
   Rule,
   SchematicContext,
   Tree,
   externalSchematic,
-  chain
+  chain,
+  noop,
+  apply,
+  empty,
+  mergeWith,
+  applyToSubtree
 } from '@angular-devkit/schematics';
 import {
   Schema as AngularNgNewSchema,
@@ -11,23 +17,43 @@ import {
   Style
 } from '@schematics/angular/ng-new/schema';
 
-export function ngNew(options: AngularNgNewSchema): Rule {
+export function ngNew(options: any): Rule {
   return async (_tree: Tree, _context: SchematicContext) => {
 
     const ngNewOptions: AngularNgNewSchema = {
       version: '12.0.4',
       name: options.name,
+      directory: options.name,
       routing: true,
       strict: true,
-      legacyBrowsers: true,
       style: Style.Scss,
       packageManager: PackageManager.Yarn
     }
 
     return chain([
-      externalSchematic('@schematics/angular', 'ng-new', ngNewOptions),
-      externalSchematic('@angular-eslint/schematics', 'ng-add', ngNewOptions)
+      mergeWith(
+        apply(empty(), [
+          externalSchematic('@schematics/angular', 'ng-new', ngNewOptions),
+          //undocumented but public api for passing down a subtree
+          applyToSubtree(ngNewOptions.directory || ngNewOptions.name, [
+            listTree(),
+            externalSchematic('@angular-eslint/schematics', 'ng-add', {})
+          ]),
+        ]),
+
+      )
     ]);
   };
 }
+
+function listTree(): Rule {
+  return async (_tree: Tree, _context: SchematicContext) => {
+    _tree.visit((p) => {
+      _context.logger.warn(`${p.toString()} `)
+    });
+
+    return noop();
+  };
+}
+
 
